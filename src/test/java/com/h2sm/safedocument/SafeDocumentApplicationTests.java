2edae4a6dc5a58cfce17c6b7ec735a881aa3c275
Egor Fedorenko <fedore.egor@gmail.com>
2021-12-15 17:55:09 +0300

    init commit 2

---
+++ src/test/java/com/h2sm/safedocument/SafeDocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h2sm.safedocument;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeDocumentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
