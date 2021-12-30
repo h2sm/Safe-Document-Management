75eb5071575da053f0289841dedcd5e2450a01a2
Egor Fedorenko <fedore.egor@gmail.com>
2021-12-30 23:40:47 +0300

    this js script was not that hard but WHY

--- main-service/src/main/resources/static/js/assignments_js.js
+++ main-service/src/main/resources/static/js/assignments_js.js
@@ -5,13 +5,21 @@ $(function () {
             url: "/assignments/getAll",
             contentType: "application/json; charset=utf-8",
             data: null,
-            success: function (data) {
-                console.log(data);
-                var $response = $("#response");
-                data.forEach(a => {
-                    $response.html(data);
-                })
-                //$response.empty().text(data);
+            success: function (response) {
+                console.log(response);
+                var trHTML = '';
+                $("tr:has(td)").remove();
+                $.each(response, function (i, item) {
+                    trHTML += '<tr><td>' + item.assignmentID +
+                        '</td><td>' + item.dateOfAssign +
+                        '</td><td>' + item.listOfDocumentsConnectedToAssignment +
+                        '</td><td>' + item.whoAssigned.name +
+                        '</td><td>' + item.whoClosedAssignment +
+                        '</td><td>' + item.whoWasAssignee.name +
+                        +'</td></tr>';
+                });
+                $('#assignmentTable').append(trHTML);
+
             },
             error: function (d) {
                 console.log(d);
