2e6b3e46b509b0be0b7f6077e81eacd9afeac68b
Egor Fedorenko <fedore.egor@gmail.com>
2022-01-07 23:43:51 +0300

    i am unironically thinking about suicide

--- src/main/java/com/h2sm/mainservice/dtos/Position.java
+++ src/main/java/com/h2sm/mainservice/dtos/Position.java
@@ -1,5 +1,7 @@
 package com.h2sm.mainservice.dtos;
 
+import javax.persistence.Entity;
+
 public enum Position {
     Director,
     HeadOfDepartment,
--- src/main/java/com/h2sm/mainservice/dtos/Worker.java
+++ src/main/java/com/h2sm/mainservice/dtos/Worker.java
@@ -2,19 +2,24 @@ package com.h2sm.mainservice.dtos;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Data
 @Entity
 public class Worker implements Serializable {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "uid")
     private long id;
-    @
+    @Column(name = "uname")
     private String name;
+    @Column(name = "usurname")
     private String surname;
+    @Column(name = "uemail")
     private String email;
+    @Column(name = "upasswd")
     private String password;
+    @Enumerated(EnumType.STRING)
     private Position position;
 }
