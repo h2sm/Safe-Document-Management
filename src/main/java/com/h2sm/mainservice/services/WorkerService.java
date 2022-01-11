a05f8060bc74bb727a0f2025608013b4cb177878
Egor Fedorenko <fedore.egor@gmail.com>
2022-01-12 00:02:21 +0300

    adding a new service i just dont give a fuck about

--- src/main/java/com/h2sm/mainservice/services/WorkerService.java
+++ src/main/java/com/h2sm/mainservice/services/WorkerService.java
@@ -5,18 +5,33 @@ import com.h2sm.mainservice.dtos.Worker;
 import com.h2sm.mainservice.repository.WorkerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class WorkerService {
     public final WorkerRepository repo;
 
-    public List<Worker> getAllWorkers(){
+    public List<Worker> getAllWorkers() {
         return repo.getAllWorkers();
     }
-    public List<Worker> getWorkersWithThisPosition(Position position){
+
+    public List<Worker> getWorkersWithThisPosition(Position position) {
         return repo.getWorkerWithThisPosition(position.name());
+    }
+
+    public Worker getWorkerById(Long id) {
+        return repo.getById(id);
+    }
+
+    public void addWorker(Worker worker) {
+        repo.save(worker);
+    }
+
+    public void deleteWorker(Worker worker) {
+        repo.delete(worker);
     }
 }
