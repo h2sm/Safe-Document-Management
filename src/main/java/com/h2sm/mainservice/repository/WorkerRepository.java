0fd6faf7d218b2d71adb4e10269419554ce353c0
Egor Fedorenko <fedore.egor@gmail.com>
2022-01-11 23:51:57 +0300

    All these bitches know I get dough (beep, beep) Pop a nigga soul like Crisco (bang, bang) I'm smokin' on the gas like Citgo (dope) Fat ass on my bitch though (yeah, yeah)

--- src/main/java/com/h2sm/mainservice/repository/WorkerRepository.java
+++ src/main/java/com/h2sm/mainservice/repository/WorkerRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 @Repository
 public interface WorkerRepository extends JpaRepository<Worker, Long> {
     @Query(value = "select * from worker where uemail = ?1", nativeQuery = true)
     Optional<Worker> findByEmail(String email);
+    @Query(value = "select * from worker", nativeQuery = true)
+    List<Worker> getAllWorkers();
+    @Query(value = "select * from worker where uposition = ?1", nativeQuery = true)
+    List<Worker> getWorkerWithThisPosition(String position);
 }
--- src/main/java/com/h2sm/mainservice/services/WorkerService.java
+++ src/main/java/com/h2sm/mainservice/services/WorkerService.java
@@ -1,11 +1,22 @@
 package com.h2sm.mainservice.services;
 
+import com.h2sm.mainservice.dtos.Position;
+import com.h2sm.mainservice.dtos.Worker;
 import com.h2sm.mainservice.repository.WorkerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class WorkerService {
     public final WorkerRepository repo;
+
+    public List<Worker> getAllWorkers(){
+        return repo.getAllWorkers();
+    }
+    public List<Worker> getWorkersWithThisPosition(Position position){
+        return repo.getWorkerWithThisPosition(position.name());
+    }
 }
